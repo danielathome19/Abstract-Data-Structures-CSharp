7ff3937ae06ccb2c4884ba8cbaa837d20aaa6b58
Zero Hour <zerohour998@gmail.com>
2017-04-07 13:54:27 -0500

    Update

--- CS Abstract Data Structures/AbstractLists.cs
+++ CS Abstract Data Structures/AbstractLists.cs
@@ -5,6 +5,9 @@
  * Created: 4/7/17
  * Purpose: A collection of data structures written in C# using generics.
  * 
+ * Current implemented:
+ *  -Linked List
+ *  -Doubly Linked List
  * 
  **************************************************************************/
 
@@ -231,6 +234,129 @@ namespace Adscol
         public bool isEmpty()
         {
             return (this.size() == 0);
+        }
+    }
+
+    class DoublyLinkedList<T>
+    {
+        //TODO: add remove(index, add(index), set(index)
+        private Node<T> myList;
+
+        public DoublyLinkedList()
+        {
+            myList = null;
+        }
+
+        public DoublyLinkedList(T t)
+        {
+            myList = new Node<T>(t);
+        }
+
+        public void add(T t)
+        {
+            Node<T> temp = new Node<T>(t);
+            if (myList == null)
+            {
+                myList = temp;
+                myList.myNext = myList;
+                myList.myPrev = myList;
+            } else
+            {
+                temp.myNext = myList;
+                temp.myPrev = myList.myPrev;
+                myList.myPrev = temp;
+                temp.myPrev.myNext = temp;
+            }
+        }
+
+        public void removeLast()
+        {
+            myList.myPrev = myList.myPrev.myPrev;
+            myList.myPrev.myNext = myList;
+            return;
+        }
+
+        public void removeFirst()
+        {
+            Node<T> temp = myList.myNext;
+            temp.myPrev = myList.myPrev;
+            temp.myNext = myList.myNext.myNext;
+            myList.myPrev.myNext = temp;
+            myList = temp;
+        }
+
+        public T get(int index)
+        {
+            int cnt = 0;
+            Node<T> temp = myList;
+            if (cnt == index)
+            {
+                return temp.myObj;
+            } else
+            {
+                cnt++;
+                if (cnt == index) return temp.myNext.myObj;
+            }
+            temp = temp.myNext;
+
+            while ((temp != myList) && (cnt <= index))
+            {
+                if (cnt == index) return temp.myObj;
+                temp = temp.myNext;
+                cnt++;
+            }
+            if (cnt == index) return temp.myObj;
+            return default(T);
+        }
+
+        public System.Collections.Generic.List<T> getList()
+        {
+            var items = new System.Collections.Generic.List<T>();
+
+            for (int i = 0; i < this.size(); i++)
+            {
+                items.Add(this.get(i));
+            }
+            return items;
+        }
+
+        public bool contains(T t)
+        {
+            var items = getList();
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (items[i].Equals(t)) return true;
+            }
+            return false;
+        }
+
+        public void clear()
+        {
+            myList = null;
+        }
+
+        public int size()
+        {
+            if (myList == null) return 0;
+
+            int cnt = 0;
+            Node<T> temp = myList;
+            cnt++;
+            temp = myList.myNext;
+
+            while (temp != myList)
+            {
+                cnt++;
+                temp = temp.myNext;
+            }
+
+            return cnt;
+        }
+
+        public bool isEmpty()
+        {
+            return this.size() == 0;
         }
     }
 }
