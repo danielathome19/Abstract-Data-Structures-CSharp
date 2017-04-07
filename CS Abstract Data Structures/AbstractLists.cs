cca19fd30bd39bc3a14671c11e42f263d6222dd1
Zero Hour <zerohour998@gmail.com>
2017-04-07 13:43:35 -0500

    Update

--- CS Abstract Data Structures/AbstractLists.cs
+++ CS Abstract Data Structures/AbstractLists.cs
@@ -39,5 +39,198 @@ namespace Adscol
             Node<T> temp = new Node<T>(t);
             myLast.myNext = temp;
         }
+
+        public LinkedList()
+        {
+            myList = null;
+            myLast = null;
+        }
+
+        public LinkedList(T t)
+        {
+            myList = new Node<T>(t);
+            myLast = myList;
+        }
+
+        public void add(T t)
+        {
+            if (myList == null)
+            {
+                Node<T> temp = new Node<T>(t);
+                myList = temp;
+                myLast = temp;
+            } else
+            {
+                this.addLast(t);
+            }
+        }
+
+        public void add(int index, T t)
+        {
+            if (index == 0)
+            {
+                Node<T> temp = new Node<T>(t);
+                temp.myNext = myList;
+                myList = temp;
+            } else if (index >= this.size() - 1)
+            {
+                this.addLast(t);
+            } else
+            {
+                int cnt = 0;
+                myLast = myList;
+                Node<T> temp = new Node<T>(t);
+                while ((myLast.myNext != null) && (cnt <= index))
+                {
+                    if (cnt == (index - 1))
+                    {
+                        temp.myNext = myLast.myNext;
+                        myLast.myNext = temp;
+                    }
+                    cnt++;
+                    myLast = myLast.myNext;
+                }
+            }
+        }
+
+        public void remove(int index)
+        {
+            if (index == 0)
+            {
+                myLast = myList;
+                Node<T> temp = myLast.myNext;
+                myList = temp;
+                myLast = myList;
+            }
+            else if (index == (this.size() - 1))
+            {
+                myLast = myList;
+                while (myLast.myNext.myNext != null)
+                {
+                    myLast = myLast.myNext;
+                }
+                myLast.myNext = null;
+            }
+            else
+            {
+                int cnt = 0;
+                myLast = myList;
+                while ((myLast.myNext != null) && (cnt <= index))
+                {
+                    if (cnt == (index - 1))
+                    {
+                        Node<T> temp = myLast;
+                        temp.myNext = myLast.myNext.myNext;
+                        myLast = temp;
+                    }
+                    cnt++;
+                    myLast = myLast.myNext;
+                }
+            }
+        }
+
+        public void set(int index, T t)
+        {
+            Node<T> temp = new Node<T>(t);
+            if (index == 0)
+            {
+                temp.myNext = myList.myNext;
+                myList = temp;
+                myLast = myList;
+            }
+            else if (index == (this.size() - 1))
+            {
+                myLast = myList;
+                while (myLast.myNext.myNext != null)
+                {
+                    myLast = myLast.myNext;
+                }
+                myLast.myNext = temp;
+            }
+            else
+            {
+                int cnt = 0;
+                myLast = myList;
+                while ((myLast.myNext != null) && (cnt <= index))
+                {
+                    if (cnt == (index - 1))
+                    {
+                        temp.myNext = myLast.myNext.myNext;
+                        myLast.myNext = temp;
+                    }
+                    cnt++;
+                    myLast = myLast.myNext;
+                }
+            }
+        }
+        
+        public T get(int index)
+        {
+            int cnt = 0;
+            myLast = myList;
+            while ((myLast.myNext != null) && (cnt <= index))
+            {
+                if (cnt == index) return myLast.myObj;
+                myLast = myLast.myNext;
+                cnt++;
+            }
+            if (cnt == index) return myLast.myObj;
+            return default(T);
+        }
+
+        public void print()
+        {
+            myLast = myList;
+            while (myLast != null)
+            {
+                Console.WriteLine(myLast.myObj);
+                myLast = myLast.myNext;
+            }
+        }
+
+        public System.Collections.Generic.List<T> getList()
+        {
+            var items = new System.Collections.Generic.List<T>();
+
+            for (int i = 0; i < this.size(); i++)
+            {
+                items.Add(this.get(i));
+            }
+            return items;
+        }
+
+        public bool contains(T t)
+        {
+            var items = this.getList();
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (items[i].Equals(t)) return true;
+            }
+            return false;
+        }
+
+        public void clear()
+        {
+            myList = null;
+            myLast = null;
+        }
+
+        public int size()
+        {
+            int cnt = 0;
+            myLast = myList;
+            while (myLast != null)
+            {
+                cnt++;
+                myLast = myLast.myNext;
+            }
+            return cnt;
+        }
+
+        public bool isEmpty()
+        {
+            return (this.size() == 0);
+        }
     }
 }
