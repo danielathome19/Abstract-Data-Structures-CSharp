df34d218f53b542dc5306ffabfbafe22359ec72e
Zero Hour <zerohour998@gmail.com>
2017-04-07 13:19:05 -0500

    Add project files.

---
+++ CS Abstract Data Structures/AbstractLists.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/*
+ * Author:  Daniel Szelogowski (C) 2017
+ * Created: 4/7/17
+ * Purpose: A collection of data structures written in C# using generics.
+ * 
+ * 
+ **************************************************************************/
+
+namespace Adscol
+{
+    class Node<T>
+    {
+        public T myObj;
+        public Node<T> myPrev;
+        public Node<T> myNext;
+
+        public Node(T t)
+        {
+            myObj = t;
+            myNext = null;
+            myPrev = null;
+        }
+    }
+
+    class LinkedList<T>
+    {
+        private Node<T> myList;
+        private Node<T> myLast;
+
+        private void addLast(T t)
+        {
+            myLast = myList;
+            while (myList.myNext != null)
+            {
+                myLast = myLast.myNext;
+            }
+            Node<T> temp = new Node<T>(t);
+            myLast.myNext = temp;
+        }
+    }
+}
---
+++ CS Abstract Data Structures/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Adscol;
+
+namespace CS_Abstract_Data_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> temp = new Node<int>(5);
+        }
+    }
+}
